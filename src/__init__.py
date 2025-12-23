ccaadd21c999b6993c0f92148580116baf9edb93
dzungpham <23127003@student.hcmus.edu.vn>
2025-12-23 18:34:52 +0700

    modify init py file

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,9 @@
 from .config_loader import load_config_from_json, post_process_config
 from .utils import configure_environment_paths, auto_unzip_colab_content, generate_test_id
 from .data_generator import create_test_datasets, create_perplexity_data, load_and_preprocess_data, create_reasoning_primitives_data, format_5_shot_prompt
-from .model import OuroThinkingExperiment, OuroBatchExperiment
+from .new_model import SafeOuroThinkingExperiment
 from .safe_model import SafeOuroThinkingExperiment, SafeOuroBatchExperiment
-from .runner import run_batch_experiment, run_holistic_evaluation
+from .new_runner import run_batch_experiment, run_holistic_evaluation
 from .evaluation_metrics import analyze_experiment_results
 __all__ = [
     "load_config_from_json", "post_process_config",
